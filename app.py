ab600ed4b33cea3c249130e3f444ba096770c7eb
Jemimah Sai <jmsai@trends.com.ph>
2019-06-20 15:53:05 +0800

    Add CORS feature to app

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ from error.handler import handle_internal_server_error
 
 from flask import Flask, Blueprint
 from flask_restful import Api
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
